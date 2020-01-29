a067735c6233a891a2b3875c21eda0b6a7bcd50d
Katsumi Kokuzawa <katsumi.kokuzawa@me.com>
2020-01-29 14:32:07 +0900

    Update: [TR-1]ログイン画面追加

--- app_view.py
+++ app_view.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
 from rest import routes as rest_routes
-from views import routes as view_routes
 
-app = Flask(__name__)
-app.register_blueprint(view_routes.module_api, url_prefix='/')
+app = Flask(__name__, static_folder='./build/static', template_folder='./build')
 app.register_blueprint(rest_routes.module_api, url_prefix='/rest')
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
